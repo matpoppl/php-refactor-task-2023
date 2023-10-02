174673d09a1429bb8789fd1a54f27c3b0abb2d4c
matpoppl <mateusz.popielarz@gmail.com>
2023-10-02 20:54:00 +0200

    separate logic by item name

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,55 +4,58 @@ namespace App;
 
 final class GildedRose
 {
-    public function updateQuality($item)
+    public function updateAged(Item $item) : void
     {
-        if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($item->quality > 0) {
-                if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                    $item->quality = $item->quality - 1;
-                } else {
-                    $item->quality = 80;
-                }
-            }
+        if ($item->sell_in < 1) {
+            $item->quality = $item->quality + 2;
         } else {
-            if ($item->quality < 50) {
-                $item->quality = $item->quality + 1;
-                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->sell_in < 11) {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                    if ($item->sell_in < 6) {
-                        if ($item->quality < 50) {
-                            $item->quality = $item->quality + 1;
-                        }
-                    }
-                }
-            }
+            $item->quality = $item->quality + 1;
         }
-
-        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-            $item->sell_in = $item->sell_in - 1;
+        
+        $item->sell_in = $item->sell_in - 1;
+        
+        $item->quality = min(50, $item->quality);
+    }
+    
+    public function updateBackstage(Item $item) : void
+    {
+        if ($item->sell_in < 1) {
+            $item->quality = 0;
+        } else if ($item->sell_in < 6) {
+            $item->quality = $item->quality + 3;
+        } else if ($item->sell_in < 11) {
+            $item->quality = $item->quality + 2;
+        } else {
+            $item->quality = $item->quality + 1;
         }
+        
+        $item->sell_in = $item->sell_in - 1;
+        
+        $item->quality = min(50, $item->quality);
+    }
+    
+    public function updateQuality(Item $item) : void
+    {
+        switch ($item->name) {
+            case 'Sulfuras, Hand of Ragnaros':
+                // do nothing
+                return;
+            case 'Aged Brie':
+                $this->updateAged($item);
+                return;
+            case 'Backstage passes to a TAFKAL80ETC concert':
+                $this->updateBackstage($item);
+                return;
+        }
+        
+        $item->quality = max(0, $item->quality - 1);
+        
+        $item->sell_in = $item->sell_in - 1;
 
         if ($item->sell_in < 0) {
-            if ($item->name != 'Aged Brie') {
-                if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if ($item->quality > 0) {
-                        if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                            $item->quality = $item->quality - 1;
-                        }
-                    }
-                } else {
-                    $item->quality = $item->quality - $item->quality;
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                }
+            if ($item->quality > 0) {
+                $item->quality = $item->quality - 1;
             }
         }
     }
-
 }
