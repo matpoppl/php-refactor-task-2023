a1cdbd744dd373a0a1e35ecaa278ee25ed9efbdb
matpoppl <mateusz.popielarz@gmail.com>
2023-10-03 19:59:01 +0200

    move constructor to abstract class in range processors

--- src/Quality/QualityTresholdProcessor.php
+++ src/Quality/QualityTresholdProcessor.php
@@ -4,43 +4,8 @@ namespace App\Quality;
 
 use App\Item;
 
-class QualityTresholdProcessor implements ProcessorInterface
+class QualityTresholdProcessor extends AbstractRangeProcessor implements ProcessorInterface
 {
-    private ?int $min = null;
-    private ?int $max = null;
-    private ActionType $action;
-    private int $value;
-    
-    /**
-     * 
-     * @param array $options
-     * 
-     * @throws \UnexpectedValueException
-     */
-    public function __construct(array $options)
-    {
-        $this->min = $options['min'] ?? null;
-        $this->max = $options['max'] ?? null;
-        $this->value = $options['value'] ?? null;
-        
-        $action = $options['action'] ?? null;
-        
-        switch ($action) {
-            case ActionType::INCREMENT:
-            case ActionType::DECREMENT:
-            case ActionType::SET:
-                $this->action = $action;
-                // ok
-                break;
-            default:
-                throw new \UnexpectedValueException("Unsuported action type `{$this->action}`");
-        }
-        
-        if (null === $this->value) {
-            throw new \UnexpectedValueException("Value option required");
-        }
-    }
-    
     public function supports(Item $item) : bool
     {
         // is below min
